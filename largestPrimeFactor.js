588a1fd98c282a51136fc9163e88a9c648b7e7c7
Rina Toyoshiba <rtoyoshiba@gmail.com>
2019-07-13 10:29:07 -1000

    prime factor

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,15 @@
 exports.largestPrimeFactor = function(n) {
   let primeNumber = 0;
   // do your work here
+  let divisor = 2;
+  while (n > 1) {
+    if (n % divisor === 0) {
+      n /= divisor;
+    } else {
+      divisor++;
+    }
+    primeNumber = divisor;
+  }
 
   return primeNumber;
 };
